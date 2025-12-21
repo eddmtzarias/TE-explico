a883cdfbb959caca8fcc2d5777c9e584c5050e89
Raggazzo <240259566+eddmtzarias@users.noreply.github.com>
2025-12-21 14:23:39 -0600

    Crear módulo shared de OmniMaestro con utilidades compartidas

---
+++ omnimastro/shared/__init__.py
@@ -0,0 +1,13 @@
+"""
+OmniMaestro Shared - Código Compartido
+
+Módulo de utilidades y código compartido entre todas las plataformas:
+- Utilidades comunes
+- Configuración compartida
+- Helpers y constantes
+- Validadores
+"""
+
+__version__ = "0.1.0"
+
+__all__ = []
